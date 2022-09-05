9525617469b030d09b31882fb336e94f4dff9824
kishore reddy <kishorekumarreddykadapa@gmail.com>
2022-09-05 23:00:34 +0530

    Application First commit

---
+++ src/main/java/com/sk/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sk.blog.springbootblogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+	}
+
+}
