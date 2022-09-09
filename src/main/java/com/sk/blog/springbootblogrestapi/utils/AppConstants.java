9ea2cb9c7247a11669533967ff2770a5f98153e8
Kishore Kumar Reddy K <kishorekumarreddykadapa@gmail.com>
2022-09-09 14:53:18 +0530

    Added Exception Class and Utils

---
+++ src/main/java/com/sk/blog/springbootblogrestapi/utils/AppConstants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.sk.blog.springbootblogrestapi.utils;
+
+/**
+ * @author kishore.kumarreddy
+ *
+ */
+public class AppConstants {
+
+	public static final String DEFAULT_PAGE_NUMBER = "0";
+	public static final String DEFAULT_PAGE_SIZE = "10";
+	public static final String DEFAULT_SORT_BY = "id";
+	public static final String DEFAULT_SORT_DIRECTION = "asc"; // asc - ascending , desc - descending
+}
